1682a6807d82e581ef57b8f4f9c0afa1abb7ea65
Jason Holloway <jason@sorted.com>
2020-08-04 19:44:50 +0100

    markdown preview stuff

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -113,6 +113,42 @@
 (use-package lsp-mode)
 
 
+;; markdown
+(defun md2html (buffer)
+  "Convert BUFFER of Markdown into a html string."
+  (princ
+   (with-temp-buffer
+     (let ((tmp (buffer-name)))
+       (set-buffer buffer)
+       (set-buffer (markdown tmp))
+       (format "<!DOCTYPE html><html><title>Markdown preview</title><link rel=\"stylesheet\" href = \"https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/3.0.1/github-markdown.min.css\"/>
+<body><article class=\"markdown-body\" style=\"box-sizing: border-box;min-width: 200px;max-width: 980px;margin: 0 auto;padding: 45px;\">%s</article></body></html>" (buffer-string))))
+  (current-buffer)))
+
+(defun md-preview ()
+  "Preview markdown file"
+  (interactive)
+  (unless (process-status "httpd")
+    (httpd-start))
+  (impatient-mode)
+  (imp-set-user-filter 'md2html)
+  (imp-visit-buffer))
+
+(use-package simple-httpd
+  :quelpa
+  :config (setq httpd-port 1777))
+
+(use-package markdown-mode
+  :quelpa
+  :mode ("\\.md\\'" . gfm-mode)
+  :commands (markdown-mode gfm-mode)
+  :config
+    (setq markdown-command "pandoc -t html5"))
+
+(use-package impatient-mode
+  :quelpa)
+
+
 ;; go
 (use-package go-mode
   :requires lsp-mode
