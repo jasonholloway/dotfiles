4e2e4c9c28fe63570fb393cbe30d63176aea4ee7
Jason Holloway <miceeatnicerice@hotmail.com>
2021-04-12 23:49:57 +0100

    rust tweaks: all-targets

--- .emacs.d/init.d/rust.el
+++ .emacs.d/init.d/rust.el
@@ -2,7 +2,6 @@
 (defvar rust-src-path "/home/jason/.rustup/toolchains/nightly-x86_64-pc-windows-gnu/lib/rustlib/src/rust/library")
 
 (use-package rustic
-  :ensure
   :after lsp-mode
   :bind (:map rustic-mode-map
               ("M-j" . lsp-ui-imenu)
@@ -13,32 +12,29 @@
               ("C-c C-c q" . lsp-workspace-restart)
               ("C-c C-c Q" . lsp-workspace-shutdown)
               ("C-c C-c s" . lsp-rust-analyzer-status))
-  :custom
-  (rustic-racer-rust-src-path rust-src-path)
   :config
   (add-to-list 'auto-mode-alist '("\\.rs\\'" . rustic-mode)))
 
-;; (use-package racer
-;;   :ensure
-;;   :custom
-;;   (racer-rust-src-path rust-src-path))
-
 (use-package lsp-ui
-  :ensure
   :after lsp-mode
   :commands lsp-ui-mode
   :custom
+  (lsp-ui-doc-hide t)
+  (lsp-ui-sideline-toggle-symbols-info nil)
   (lsp-ui-peek-always-show t))
 
 (use-package lsp-mode
-  :ensure
   :commands lsp
-  :hook (lsp-mode . (lambda ()
-                      (let ((lsp-keymap-prefix "M-l"))
-                        (lsp-enable-which-key-integration))))
+  :hook
+  (lsp-mode . (lambda ()
+                (let ((lsp-keymap-prefix "M-l"))
+                  (lsp-enable-which-key-integration))))
   :custom
   (lsp-rust-analyzer-server-display-inlay-hints nil)
-  (lsp-eldoc-render-all t)
+  (lsp-eldoc-render-all nil)
+  :init
+  (setq lsp-rust-all-targets t)
+  (setq lsp-rust-analyzer-cargo-all-targets t)
   :config
   (define-key lsp-mode-map (kbd "M-l") lsp-command-map)
   (add-hook 'lsp-mode-hook 'lsp-ui-mode))
